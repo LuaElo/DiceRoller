b76d9a8a54330764ee8600674caa4b8100ebe730
LuaElo <124682895+LuaElo@users.noreply.github.com>
2024-04-02 12:41:57 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        Random gerar = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Clicar(object sender, EventArgs e) // botão
+        {
+                int gerarNumero = gerar.Next(1,11);
+                gerarNumeroLabel.Text = $"{gerarNumero}";
+           
+        }
+
+      
+
+
+
+
+    }
+}
