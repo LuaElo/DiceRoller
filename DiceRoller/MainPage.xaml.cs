3ba8768949830a463394b22eb9bc82c738570531
Eloah e Luara <eloaheluaraa123@gmail.com>
2024-04-09 12:30:15 -0300

    Correção prova

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Clicar(object sender, EventArgs e) // botão
+        {
+            //Pegar o numero que o usuario escolheu
+            int selectSides = Convert.ToInt32(qtdDados.SelectedItem);
+            //usar esse numero com "limitador do metodo Random"
+            //sortear um numero utilizado o metod random
+            Random gerar = new Random();
+            int gerarNumero = gerar.Next(1, selectSides + 1);
+            //pegar esse resultado e exibir na label 
+              gerarNumeroLabel.Text = gerarNumero.ToString();
+           
+        }
+
+      
+
+
+
+
+    }
+}
